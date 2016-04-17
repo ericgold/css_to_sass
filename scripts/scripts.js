d872f8888e1e67571ab5d2db1ca2a670e0d5773c
ericgold <egold05@gmail.com>
2016-04-17 09:03:20 -0700

    toggle button and adjustments
    changed button script to simply hide the nav rather than apply the
    class ‘is-collapsed-mobile’ which was not working
    
    made flexy mixin’s “align-items’ optional by setting default to null
    
    added placeholders margin-standard and padding-standard for reused
    spacing styles
    
    styled toggle button a bit
    
    creative font-size-default variable and applied it to p element

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,4 @@
 $('#toggle').click(function() {
-	$(this).next('.main-nav').children('a').toggleClass("is-collapsed-mobile");
+	$(this).next('.main-nav').children().toggle();
 });
+//toggleClass('is-collapsed-mobile');
