6f8eaa67f65fe772a0f1b4919ed07dcc013206d4
ericgold <egold05@gmail.com>
2016-04-18 15:12:37 -0700

    hide menu by default on mobile
    added script to hide the menu by default on mobile screen widths
    (<768). Then menu can be toggled on and off with hamburger icon as
    before.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,12 @@
+$(document).ready(function(){
+	if ($(document).width() < 768) {
+		$('.main-nav').hide();
+	} else {
+		$('.main-nav').show();
+	}
+});
+
+
 $('#toggle').click(function() {
-	$(this).next('.main-nav').children().toggle();
+	$(this).next('.main-nav').toggle();
 });
